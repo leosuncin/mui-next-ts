3ab7ea028781cf471c7ecaae9e651f48d0e86c60
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-12-29 13:52:58 -0600

    fix(build): Move styles inside page

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,12 +4,44 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
 import LoginForm from 'components/forms/login';
-import useStyles from './styles';
 import { useAuth } from 'hooks/auth-context';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/random)',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'dark'
+          ? theme.palette.grey[900]
+          : theme.palette.grey[50],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    errorMessage: {
+      textAlign: 'center',
+      color: '#e51560',
+    },
+  }),
+);
 
 export default function SignInSide(props) {
   const [error, setError] = useState<string>();
