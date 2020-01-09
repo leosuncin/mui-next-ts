790752261da22cf7654fd92a4af2614725ddf82f
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-01-09 16:44:03 -0600

    feat(register): Add register page

---
+++ __tests__/pages/register.spec.tsx
@@ -0,0 +1,86 @@
+import { act, fireEvent, render, waitForElement } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+const spyRouterPush = jest.fn();
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      push: spyRouterPush,
+    };
+  },
+}));
+
+import RegisterPage from 'pages/register';
+import { UserProvider } from 'hooks/user-context';
+import { AuthProvider } from 'hooks/auth-context';
+
+describe('<RegisterPage />', () => {
+  /* global fetchMock */
+  let tree;
+
+  beforeEach(() => {
+    tree = (
+      <UserProvider>
+        <AuthProvider>
+          <RegisterPage />
+        </AuthProvider>
+      </UserProvider>
+    );
+  });
+
+  afterEach(() => {
+    spyRouterPush.mockReset();
+  });
+
+  it('should render', () => {
+    expect(render(tree)).toBeDefined();
+  });
+
+  it('should shown the error for duplicate user', async () => {
+    fetchMock.mockResponse(
+      `{
+      "statusCode": 409,
+      "error": "Conflict",
+      "message": "Username or Email already registered"
+    }`,
+      { status: 409 },
+    );
+    const { getByLabelText, getByRole, getByText } = render(tree);
+
+    userEvent.type(getByLabelText(/First name/i), 'Jane');
+    userEvent.type(getByLabelText(/Last name/i), 'Doe');
+    userEvent.type(getByLabelText(/Email/i), 'jane@doe.me');
+    userEvent.type(getByLabelText(/Password/i), '!drowssap');
+
+    await act(async () => {
+      fireEvent.submit(getByRole('form'));
+    });
+
+    await expect(
+      waitForElement(() => getByText('Username or Email already registered')),
+    ).resolves.toBeInTheDocument();
+  });
+
+  it('should register a new user', async () => {
+    fetchMock.mockResponse(`{
+      "id": "760add88-0a2b-4358-bc3f-7d82245c5dea",
+      "username": "kristen.williams@example.com",
+      "name": "Kristen Williams",
+      "picture": "https://i.pravatar.cc/200",
+      "bio": "Lorem ipsum dolorem"
+    }`);
+    const { getByLabelText, getByRole } = render(tree);
+
+    userEvent.type(getByLabelText(/First name/i), 'Kristen');
+    userEvent.type(getByLabelText(/Last name/i), 'Williams');
+    userEvent.type(getByLabelText(/Email/i), 'kristen.williams@example.com');
+    userEvent.type(getByLabelText(/Password/i), 'Pa$$w0rd!');
+
+    await act(async () => {
+      fireEvent.submit(getByRole('form'));
+    });
+
+    expect(spyRouterPush).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ pages/register.tsx
@@ -0,0 +1,86 @@
+import Avatar from '@material-ui/core/Avatar';
+import Fade from '@material-ui/core/Fade';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+
+import RegisterForm from 'components/forms/register';
+import { useAuth } from 'hooks/auth-context';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/random)',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'dark'
+          ? theme.palette.grey[900]
+          : theme.palette.grey[50],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    errorMessage: {
+      textAlign: 'center',
+      color: '#e51560',
+    },
+  }),
+);
+
+export default function SignInSide(props) {
+  const [error, setError] = useState<string>();
+  const { register } = useAuth();
+  const router = useRouter();
+  const classes = useStyles(props);
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome aboard
+          </Typography>
+          <Fade in={!!error} aria-expanded={!!error}>
+            <Typography color="secondary" className={classes.errorMessage}>
+              <span role="img" aria-label="sad face">
+                😥
+              </span>
+              &nbsp;{error}
+            </Typography>
+          </Fade>
+          <RegisterForm
+            onSubmit={async body => {
+              try {
+                setError(null);
+                await register(body);
+                router.push('/');
+              } catch (error) {
+                setError(error.message);
+              }
+            }}
+          />
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
