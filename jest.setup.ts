829e8d28beb766d2f07a33cef871ff219bde895e
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-01-11 16:32:23 -0600

    chore: Update jest-fetch-mock setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,6 @@
+/* eslint-env jest */
 import '@testing-library/jest-dom/extend-expect';
-import { GlobalWithFetchMock } from 'jest-fetch-mock';
+require('jest-fetch-mock').enableMocks();
 import 'mutationobserver-shim';
-
-const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
-customGlobal.fetch = require('jest-fetch-mock');
-customGlobal.fetchMock = customGlobal.fetch;
 
 jest.setMock('cross-fetch', fetch);
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+import 'jest-fetch-mock';
+
 declare namespace NodeJS {
   export interface ProcessEnv {
     readonly PORT: string;
