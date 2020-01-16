4daebd556d0189c51f621bf5100054e77a8746c1
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-01-16 13:34:26 -0600

    chore(next): Remove @zeit/next-css after upgrade next.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
-const withCss = require('@zeit/next-css');
 const withFonts = require('next-fonts');
 
-module.exports = withFonts(
-  withCss({
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty',
-      };
-      config.resolve.modules.push(__dirname);
+module.exports = withFonts({
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty',
+    };
+    config.resolve.modules.push(__dirname);
 
-      return config;
-    },
-    env: {
-      APP_SECRET:
-        process.env.APP_SECRET || 'MOdfbiPLnR5JjH9L3gTJXUmwm8tRhU8znowuOIQj',
-    },
-  }),
-);
+    return config;
+  },
+  env: {
+    APP_SECRET:
+      process.env.APP_SECRET || 'MOdfbiPLnR5JjH9L3gTJXUmwm8tRhU8znowuOIQj',
+  },
+});
