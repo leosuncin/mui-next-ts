2d06d605ef6e939c26971de7359eaafb72d8ad21
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-06-07 20:54:23 -0600

    test(e2e): Extract statechart from test

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,142 +1,42 @@
 import { createModel } from '@xstate/test';
 import faker from 'faker';
-import isEmpty from 'validator/lib/isEmpty';
-import isLength from 'validator/lib/isLength';
-import { createMachine } from 'xstate';
 
-type TypeUsernameEvent = {
-  type: string;
-  username: string;
-};
-type TypePasswordEvent = {
-  type: string;
-  password: string;
-};
-type FillEvent = TypeUsernameEvent & TypePasswordEvent;
-function isValidUsername(username: string): boolean {
-  return (
-    !isEmpty(username, { ignore_whitespace: true }) &&
-    isLength(username, { min: 5 })
-  );
-}
-function isValidPassword(password: string): boolean {
-  return (
-    !isEmpty(password, { ignore_whitespace: true }) &&
-    isLength(password, { min: 8 })
-  );
-}
-function isValidLogin({ username, password }: FillEvent): boolean {
-  return isValidUsername(username) && isValidPassword(password);
-}
-function areValidCredentials({ username, password }: FillEvent): boolean {
-  return username === 'admin' && password === 'Pa$$w0rd!';
-}
+import getMachineWithTests, {
+  FillEvent,
+} from '../../machines/login-test-machine';
+
 const usernameLabel = /Username/i;
 const passwordLabel = /Password/i;
 const submitButton = /Log me in/i;
-const failMachine = createMachine<never, FillEvent>({
-  id: 'login-fail-test',
-  initial: 'pristine',
-  states: {
-    pristine: {
-      on: {
-        FILL_FORM: [
-          {
-            target: 'invalid.username',
-            cond: (_, event) => !isValidUsername(event.username),
-          },
-          {
-            target: 'invalid.password',
-            cond: (_, event) => !isValidPassword(event.password),
-          },
-          {
-            target: 'correctCredentials',
-            cond: (_, event) => areValidCredentials(event),
-          },
-          {
-            target: 'valid',
-            cond: (_, event) => isValidLogin(event),
-          },
-        ],
-      },
-      meta: {
-        test(cy: Cypress.cy) {
-          cy.findByLabelText('sad face').should('not.be.visible');
-        },
-      },
-    },
-    invalid: {
-      states: {
-        username: {
-          meta: {
-            test(cy: Cypress.cy) {
-              cy.findByText(/Username.+(?:empty|too short)/).should('exist');
-            },
-          },
-        },
-        password: {
-          meta: {
-            test(cy: Cypress.cy) {
-              cy.findByText(/Password.+(?:empty|too short)/).should('exist');
-            },
-          },
-        },
-      },
-    },
-    valid: {
-      on: {
-        SUBMIT: { target: 'incorrectCredentials' },
-      },
-      meta: {
-        test(cy: Cypress.cy) {
-          cy.findByText(/Username.+(?:empty|too short)/).should('not.exist');
-          cy.findByText(/Password.+(?:empty|too short)/).should('not.exist');
-        },
-      },
-    },
-    correctCredentials: {
-      on: {
-        SUBMIT: { target: 'success' },
-      },
-    },
-    incorrectCredentials: {
-      on: {
-        RETRY: { target: 'retry' },
-      },
-      meta: {
-        test(cy: Cypress.cy) {
-          cy.findByText(/Wrong (?:username|password)/i).should('exist');
-        },
-      },
-    },
-    success: {
-      type: 'final',
-      meta: {
-        test(cy: Cypress.cy) {
-          cy.waitUntil(() =>
-            cy.location('pathname').then(pathname => pathname !== '/login'),
-          )
-            .location('pathname')
-            .should('equal', '/');
-        },
-      },
-    },
-    retry: {
-      on: {
-        SUBMIT: { target: 'locked' },
-      },
-    },
-    locked: {
-      type: 'final',
-      meta: {
-        test(cy: Cypress.cy) {
-          cy.findByText(/Too many failed attempts/i).should('exist');
-          cy.findByLabelText(usernameLabel).should('be.disabled');
-          cy.findByLabelText(passwordLabel).should('be.disabled');
-          cy.findByText(submitButton).parent().should('be.disabled');
-        },
-      },
-    },
+const failMachine = getMachineWithTests({
+  pristine: (cy: Cypress.cy) => {
+    cy.findByLabelText('sad face').should('not.be.visible');
+  },
+  'invalid.username': (cy: Cypress.cy) => {
+    cy.findByText(/Username.+(?:empty|too short)/).should('exist');
+  },
+  'invalid.password': (cy: Cypress.cy) => {
+    cy.findByText(/Password.+(?:empty|too short)/).should('exist');
+  },
+  valid: (cy: Cypress.cy) => {
+    cy.findByText(/Username.+(?:empty|too short)/).should('not.exist');
+    cy.findByText(/Password.+(?:empty|too short)/).should('not.exist');
+  },
+  incorrectCredentials: (cy: Cypress.cy) => {
+    cy.findByText(/Wrong (?:username|password)/i).should('exist');
+  },
+  success: (cy: Cypress.cy) => {
+    cy.waitUntil(() =>
+      cy.location('pathname').then(pathname => pathname !== '/login'),
+    )
+      .location('pathname')
+      .should('equal', '/');
+  },
+  locked: (cy: Cypress.cy) => {
+    cy.findByText(/Too many failed attempts/i).should('exist');
+    cy.findByLabelText(usernameLabel).should('be.disabled');
+    cy.findByLabelText(passwordLabel).should('be.disabled');
+    cy.findByText(submitButton).parent().should('be.disabled');
   },
 });
 const testModel = createModel(failMachine, {
