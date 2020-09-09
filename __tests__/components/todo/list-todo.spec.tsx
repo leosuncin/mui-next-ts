bbc19002a13b496e2ca8bcc7d714dbf70e2c59ef
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-08 22:18:52 -0600

    test(todo): Add unit test for list todo

---
+++ __tests__/components/todo/list-todo.spec.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ListTodo from 'components/todo/list-todo';
+import { random } from 'faker';
+import React from 'react';
+import { todoBuild } from 'utils/factories';
+
+describe('<ListTodo />', () => {
+  it('should render', () => {
+    expect(
+      render(
+        <ListTodo todos={[]} onChangeTodo={jest.fn} onRemoveTodo={jest.fn} />,
+      ),
+    ).toBeDefined();
+  });
+
+  it('should list todos', () => {
+    const spyChangeTodo = jest.fn();
+    const spyRemoveTodo = jest.fn();
+    const todos = Array.from({ length: 10 }, () =>
+      todoBuild({ traits: 'old' }),
+    );
+    const todo = random.arrayElement(todos);
+
+    render(
+      <ListTodo
+        todos={todos}
+        onChangeTodo={spyChangeTodo}
+        onRemoveTodo={spyRemoveTodo}
+      />,
+    );
+    userEvent.click(
+      screen
+        .getByRole('listitem', { name: RegExp(todo.text) })
+        .querySelector('input[type=checkbox]'),
+    );
+
+    expect(spyChangeTodo).toHaveBeenCalledWith(todo.id, { done: !todo.done });
+
+    userEvent.click(
+      screen.getByRole('button', { name: `Delete todo: ${todo.text}` }),
+    );
+
+    expect(spyRemoveTodo).toHaveBeenCalledWith(todo, expect.any(Number));
+  });
+});
