3c6970885256eeb34b8c373db8a2c2b0be6f36b6
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-10 17:50:15 -0600

    chore(jest): Code coverage ignore utils directory

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,12 @@ module.exports = {
   moduleDirectories: ['node_modules', '.'],
   modulePathIgnorePatterns: ['<rootDir>/cypress'],
   coverageDirectory: '<rootDir>/coverage/unit',
-  coveragePathIgnorePatterns: ['/node_modules/', 'pages/api/__coverage__.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'pages/api/__coverage__.ts',
+    '/test-server/',
+    'utils/factories.ts',
+  ],
   globals: {
     // we must specify a custom tsconfig for tests because we need the typescript transform
     // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
