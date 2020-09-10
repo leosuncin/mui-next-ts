ee3af0e2c10f3451cce48ee0e039e4fd20fba634
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-10 15:36:52 -0600

    test(todo): Add integration test to create a new todo

--- __tests__/components/todo/todo.spec.tsx
+++ __tests__/components/todo/todo.spec.tsx
@@ -3,7 +3,9 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Todo from 'components/todo';
+import faker from 'faker';
 import React from 'react';
 import server from 'utils/test-server';
 import { respondWithServiceUnavailable } from 'utils/test-server/handlers/handle-with-error';
@@ -36,5 +38,20 @@ describe('<Todo />', () => {
     expect(
       screen.getByRole('list', { name: 'List of todo' }).children,
     ).toHaveLength(10); // Check all todos are listed, by default 10
+  });
+
+  it('should create a new todo and add to list', async () => {
+    const text = faker.lorem.words();
+    render(<Todo />);
+
+    await waitForElementToBeRemoved(screen.getByTestId('loading-todos')); // Wait until loader to disappear
+
+    await userEvent.type(screen.getByRole('textbox', { name: /Text/ }), text); // Typing the new todo
+    userEvent.click(screen.getByRole('button', { name: /Add/i })); // Submit the form
+
+    await screen.findByTestId('loading-todos'); // Wait until loader appear
+    await waitForElementToBeRemoved(screen.getByTestId('loading-todos')); // Then, wait until loader to disappear
+
+    expect(screen.getByText(text)).toBeInTheDocument(); // Find the new todo in the list
   });
 });
