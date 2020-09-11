13ad43d6a7b045d006f05c67ad9addb993c61730
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-09-11 12:45:50 -0600

    test(todo): Add component test to list the todos

---
+++ cypress/component/todo.test.tsx
@@ -0,0 +1,30 @@
+import Todo from 'components/todo';
+import { mount } from 'cypress-react-unit-test';
+import React from 'react';
+import { todoBuild } from 'utils/factories';
+
+describe('Todo component', () => {
+  const todos = Array.from({ length: 10 }, () => todoBuild({ traits: 'old' }));
+  const activeCount = todos.reduce(
+    (count, todo) => count + ((!todo.done as unknown) as number),
+    0,
+  );
+  const completedCount = todos.length - activeCount;
+  const allButton = RegExp(`All \\(${todos.length}\\)`, 'i');
+  const activeButton = RegExp(`Active \\(${activeCount}\\)`, 'i');
+  const completedButton = RegExp(`Completed \\(${completedCount}\\)`, 'i');
+
+  beforeEach(() => {
+    cy.server().route('GET', '/api/todos?page=*', todos).as('listTodo');
+  });
+
+  it('should list the todos', () => {
+    mount(<Todo />);
+
+    cy.findAllByRole('listitem').its('length').should('equal', todos.length);
+    cy.findByText(RegExp(`${activeCount} items left`, 'i')).should('exist');
+    cy.findByText(allButton).should('exist');
+    cy.findByText(activeButton).should('exist');
+    cy.findByText(completedButton).should('exist');
+  });
+});
