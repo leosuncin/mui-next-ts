d9ce05c30c9e7d743a567718557b6d7bf3fc97e5
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-10-25 16:28:49 -0600

    fix(jest): Fix ts-jest config to remove warning
    Change `tsConfig` to `tsconfig` to avoid deprecation

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
     // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
     // can see this setting in tsconfig.jest.json -> "jsx": "react"
     'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
+      tsconfig: 'tsconfig.test.json',
     },
   },
 };
