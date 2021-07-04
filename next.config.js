82b5221355f1d5ca1913dbc17ea9791b3b9935f3
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-07-04 15:23:37 -0600

    build: fix next.js config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const withFonts = require('next-fonts');
 
 module.exports = withFonts({
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    };
-
-    return config;
-  },
   env: {
     APP_SECRET:
       process.env.APP_SECRET || 'MOdfbiPLnR5JjH9L3gTJXUmwm8tRhU8znowuOIQj',
