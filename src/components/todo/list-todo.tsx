c595f547932efdfe5be9f9d8da387b9d2e746dab
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-07-04 15:17:55 -0600

    fix: amend eslint errors

--- src/components/todo/list-todo.tsx
+++ src/components/todo/list-todo.tsx
@@ -5,27 +5,31 @@ import { TodoResponse as Todo, UpdateTodo } from '@app/types';
 
 import TodoItem from './todo-item';
 
-const ListTodo: React.FC<{
+export interface ListTodoProps {
   todos: Todo[];
   saving?: boolean;
   onChangeTodo: (id: string, body: UpdateTodo) => void;
   onRemoveTodo: (todo: Todo, position?: number) => void;
-}> = props => (
-  <Grid item>
-    {props.saving ? (
-      <LinearProgress variant="indeterminate" data-testid="saving-todos" />
-    ) : null}
-    <List aria-label="List of todo">
-      {props.todos.map((todo, idx) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onChangeTodo={body => props.onChangeTodo(todo.id, body)}
-          onRemoveTodo={props.onRemoveTodo.bind(null, todo, idx)}
-        />
-      ))}
-    </List>
-  </Grid>
-);
+}
+
+function ListTodo(props: ListTodoProps) {
+  return (
+    <Grid item>
+      {props.saving ? (
+        <LinearProgress variant="indeterminate" data-testid="saving-todos" />
+      ) : null}
+      <List aria-label="List of todo">
+        {props.todos.map((todo, idx) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onChangeTodo={body => props.onChangeTodo(todo.id, body)}
+            onRemoveTodo={props.onRemoveTodo.bind(null, todo, idx)}
+          />
+        ))}
+      </List>
+    </Grid>
+  );
+}
 
 export default ListTodo;
