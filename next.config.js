06452375250a357ba39e294a0039050630a4bdc8
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-09-05 21:51:34 -0600

    build(next): enable experimental ES Module support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const withFonts = require('next-fonts');
 
 module.exports = withFonts({
-  env: {
-    APP_SECRET:
-      process.env.APP_SECRET || 'MOdfbiPLnR5JjH9L3gTJXUmwm8tRhU8znowuOIQj',
+  future: {
+    esmExternals: true,
   },
 });
